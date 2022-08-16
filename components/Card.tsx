6422248177012283a1b4d453df61ebb342bbaa5c
ogeedeveloper <omarogrant202@hotmail.com>
2022-08-16 12:46:52 -0500

    Installed framer motion and implement the gestures from framer-motion in the card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image"
+import {motion} from "framer-motion"
 
 interface Props {
     // any props that come into the component
@@ -7,18 +8,25 @@ interface Props {
     size:string
 }
 
-const Card: React.FunctionComponent<Props> = ({imgUrl, size}) => {
+const Card: React.FunctionComponent<Props> = ({imgUrl="/image/image-not-found.png", size="medium"}) => {
+    const [imgSrc, setImgSrc] = useState(imgUrl)
+
     // style map
     const styleMap:any = {
         large: 'relative w-[218px] min-w-[218px] h-[434px] min-h-434px]',
         medium: 'relative w-[158px] min-w-[158px] h-[280px] min-h-[280px]',
         small: 'relative w-[300px] min-w-[300px] h-[170px] min-h-[170px]'
     }
+
+    const handleImageError = () =>{
+        console.log("error triggered")
+        setImgSrc("/image/image-not-found.png")
+    }
   return (
     <div className='mr-1  cursor-pointer'>
-        <div className={styleMap[size]}>
-            <Image src={imgUrl} alt='image' layout='fill' className='block top-0 right-0 bottom-0 left-0 rounded-md object-cover object-center max-w-full' />
-        </div>
+        <motion.div className={`${styleMap[size]} imgMotionWrapper`} whileHover={{scale:1.2}}>
+            <Image src={imgUrl} alt='image' layout='fill' className='block top-0 right-0 bottom-0 left-0 rounded-md object-cover object-center max-w-full' onError={handleImageError} />
+        </motion.div>
     </div>
   )
 }
