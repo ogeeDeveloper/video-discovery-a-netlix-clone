bf0fc588c344423e610758cf477781e98c43bdc1
ogeedeveloper <omarogrant202@hotmail.com>
2022-08-16 10:09:25 -0500

    Added the different style sizes for the card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,9 +8,17 @@ interface Props {
 }
 
 const Card: React.FunctionComponent<Props> = ({imgUrl, size}) => {
+    // style map
+    const styleMap:any = {
+        large: 'relative w-[218px] min-w-[218px] h-[434px] min-h-434px]',
+        medium: 'relative w-[158px] min-w-[158px] h-[280px] min-h-[280px]',
+        small: 'relative w-[300px] min-w-[300px] h-[170px] min-h-[170px]'
+    }
   return (
-    <div>
-        <Image src={imgUrl} alt='image' width={300} height={300} objectFit="contain" />
+    <div className='mr-1  cursor-pointer'>
+        <div className={styleMap[size]}>
+            <Image src={imgUrl} alt='image' layout='fill' className='block top-0 right-0 bottom-0 left-0 rounded-md object-cover object-center max-w-full' />
+        </div>
     </div>
   )
 }
