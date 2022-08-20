e73cb623d2a9d9f5f90cf9aba2e14ec091c2a775
ogeedeveloper <omarogrant202@hotmail.com>
2022-08-20 14:09:14 -0500

    Refactor the section card to get dynamic data as well as taking the size as a prop

--- components/SectionCards.tsx
+++ components/SectionCards.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import Card from './Card'
 
-const SectionCards = ({title}:{title:string}) => {
+const SectionCards = ({title,size,videos}:{title:string, size:string, videos:{imgUrl:string}[]}) => {
+  console.log({videos})
   return (
     <section className='text-blue20 bg-black50 px-4 md:px-16'>
         <h1 className='text-white10 font-bold text-2xl'>{title}</h1>
-        <div className='flex flex-wrap py-4 mt-6 mr-3 overflow-x-auto overflow-y-hidden'>
-          <Card imgUrl="/image/avatar.jpg" size="large" id={0} />
-          <Card imgUrl="/image/avatar.jpg" size="large" id={1}/>
-          <Card imgUrl="/image/avatar.jpg" size="large" id={2}/>
-          <Card imgUrl="/image/avatar.jpg" size="large" id={3}/>
-          <Card imgUrl="/image/avatar.jpg" size="large" id={4}/>
-          <Card imgUrl="/image/avatar.jpg" size="large" id={5}/>
-          <Card imgUrl="/image/avatar.jpg" size="large" id={6}/>
-          <Card imgUrl="/image/avatar.jpg" size="large" id={7}/>
+        <div className='flex py-4 mt-6 mr-3 overflow-x-scroll overflow-y-hidden'>
+          {videos && videos.map((video,index)=>{
+            return <Card key={index} imgUrl={video.imgUrl} size={size} id={index} />
+          })}
         </div>
     </section>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,12 @@ import Navbar from '../components/Navbar'
 import SectionCards from '../components/SectionCards'
 
 const Home: NextPage = () => {
+  // Dummydata for populating card
+  const disneyVideos = [
+    {imgUrl: "/image/avatar.jpg"},
+    {imgUrl: "/image/avatar.jpg"},
+    {imgUrl: "/image/avatar.jpg"}
+  ]
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Head>
@@ -15,7 +21,7 @@ const Home: NextPage = () => {
       <Navbar username="ogeedev@mail.com" />
       <Banner title="Avatar" subtitle="The last air bender" imgURL="/image/avatar.jpg" />
       <div className='mt-6'>
-        <SectionCards title='Disney'/>
+        <SectionCards title='Disney' videos={disneyVideos} size="large" />
       </div>
       {/* <Card imgUrl="/image/avatar.jpg" size="medium"/>
       <Card imgUrl="/image/avatar.jpg" size="small"/> */}
