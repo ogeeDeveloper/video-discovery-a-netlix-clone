f587a3aef9f076fb6ccf745fb231b21301fe59ed
ogeedeveloper <omarogrant202@hotmail.com>
2022-08-23 09:09:41 -0500

    Invoked the youtube api URL to return list of videos from search

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,7 +1,17 @@
 import videoData from "../data/test-data.json"
 
-export const getVideo = () => {
-    return videoData.items.map(item=>{
+export const getVideo = async() => {
+    const YoutubeApiKey = process.env.YOUTUBE_API
+
+    // 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=disney%20trailer&key=[YOUR_API_KEY]' \
+
+    // create fetch request that will return a promise
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=disney%20trailer&key=${YoutubeApiKey}`)
+
+    // Return the response as json
+    const data = await response.json()
+ 
+    return data?.items.map((item)=>{
         return {
             title: item.snippet.title,
             imgUrl: item.snippet.thumbnails.high.url,
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,12 @@ import SectionCards from '../components/SectionCards'
 import {getVideo} from "../lib/videos"
 
 export async function getServerSideProps() {
-    const disneyVideos = getVideo()
+    const disneyVideos = await getVideo()
     // Pass the disneyVideos as props to our home page
     return {props: {disneyVideos}}
 }
 
 const Home: NextPage = ({disneyVideos}:any) => {
-  // Dummydata for populating card
-//   const disneyVideos = [
-//     {imgUrl: "/image/avatar.jpg"},
-//     {imgUrl: "/image/avatar.jpg"},
-//     {imgUrl: "/image/avatar.jpg"}
-//   ]
-
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Head>
@@ -29,6 +22,7 @@ const Home: NextPage = ({disneyVideos}:any) => {
       <Navbar username="ogeedev@mail.com" />
       <Banner title="Shrek 5" subtitle="The last air bender" imgURL="/image/Shrek-5.jpg" />
       <div className='mt-6'>
+        <SectionCards title='Disney' videos={disneyVideos} size="large" />
         <SectionCards title='Disney' videos={disneyVideos} size="large" />
       </div>
       {/* <Card imgUrl="/image/avatar.jpg" size="medium"/>
