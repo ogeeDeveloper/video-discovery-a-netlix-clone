cdf290a08f6b011022fe4b44efcbdf4e00e23065
ogeeDeveloper <omarogrant202@hotmail.com>
2022-10-04 15:57:59 -0500

    Added structure to the Modal component

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -21,6 +21,27 @@ const Video = () => {
     const handleRequestCloseFunc = () => {
         router.back()
     }
+
+    // define video interface
+    interface Video {
+      title: string,
+      publishTime: string,
+      description: string,
+      channelTitle: string,
+      viewCount: number,
+    }
+
+    // Create structure for modal
+    const videoModal:Video = {
+      title: 'Video',
+      publishTime: '2021-01-01',
+      description: 'This is a video',
+      channelTitle: 'Channel',
+      viewCount: 100,
+    }
+
+    const {title, publishTime, description, channelTitle, viewCount}= videoModal
+    
   return (
     <div>
         <Modal
@@ -30,12 +51,37 @@ const Video = () => {
             overlayClassName={"top-0 left-0 right-0 bottom-0 w-full h-screen"}
             className={"absolute left-0 right-0 mx-auto my-0 w-800 bottom-[40px] bg-black40 top-[10%] outline-none rounded-[10px] border-2 border-solid border-shadow-2xl"}
         >
+          <iframe id="player" type="text/html" width="100%" height="390"
+              src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://localhost:3000&controls=0&showinfo=0&rel=0&autoplay=1`}
+              frameborder="0" className='rounded-sm borderBoxShadow'>
+            </iframe>
+
+          {/* Modal Body */}
+          <div>
+            {/* Modal body content */}
             <div>
-              <iframe id="player" type="text/html" width="100%" height="390"
-                src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://localhost:3000&controls=0&showinfo=0&rel=0&autoplay=1`}
-                frameborder="0" className='rounded-sm borderBoxShadow'>
-              </iframe>
+              {/* Column 1 */}
+              <div>
+                {/* Published Time */}
+                <p>{publishTime}</p>
+                {/* Title */}
+                <p>{title}</p>
+                {/* Description */}
+                <p>{description}</p>
+              </div>
+              {/* Column 2 */}
+              <div>
+                <p className='subText subTextWrapper'>
+                  <span className='infoText'>Cast: </span>
+                  <span>{channelTitle}</span>
+                </p>
+                <p className='subText subTextWrapper'>
+                  <span className='infoText'>View Count: </span>
+                  <span>{viewCount}</span>
+                </p>
+              </div>
             </div>
+          </div>
         </Modal>
     </div>
   )
