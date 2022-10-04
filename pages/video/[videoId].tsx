a4f229bc8f3a953cdb78a04ab5f6d7537610a0b7
ogeeDeveloper <omarogrant202@hotmail.com>
2022-10-04 15:26:08 -0500

    Feature: Added the styling to the video player

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -28,12 +28,12 @@ const Video = () => {
             contentLabel="Video player"
             onRequestClose={handleRequestCloseFunc}
             overlayClassName={"top-0 left-0 right-0 bottom-0 w-full h-screen"}
-            className={"absolute left-0 right-0 my-auto mx-0 w-800 buttom-[40] bg-black40 top-[10%] outline-none rounded-[10px] border-2 border-solid border-shadow-2xl"}
+            className={"absolute left-0 right-0 mx-auto my-0 w-800 bottom-[40px] bg-black40 top-[10%] outline-none rounded-[10px] border-2 border-solid border-shadow-2xl"}
         >
             <div>
-              <iframe id="player" type="text/html" width="640" height="390"
+              <iframe id="player" type="text/html" width="100%" height="390"
                 src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://localhost:3000&controls=0&showinfo=0&rel=0&autoplay=1`}
-                frameborder="0">
+                frameborder="0" className='rounded-sm borderBoxShadow'>
               </iframe>
             </div>
         </Modal>
